50ad6561f2542fe51f0b6d4f2796c48a8b2604e5
Jonas Kunath <jonas.kunath@gmx.de>
2021-08-04 16:59:50 +0200

    Improve messagesfrom rootfile loading script
    Most importantly, backticks where removed from a comment.
    With the backticks, this part of the comment would actually be executed.

--- init.sh
+++ init.sh
@@ -23,26 +23,26 @@ activate_environment
 #
 # Get the large root per-event files.
 #
-if [ "$1" = "" ]; then
-    :
-elif [ "$1" = "rootfiles" ]; then
+search_for_rootfiles () {
+    LOAD_TYPE=$1
+    local_data=$2
+
     mkdir -p $CODE_DIR/tmp
     rootfile_destination=$CODE_DIR/tmp/per_event_data
     if [ -d $rootfile_destination ]; then
-        echo "$rootfile_destination already exists. If it is empty or with wrong data, "
-        echo "please remove the folder and run this script again."
-    elif [ "$2" = "local" ]; then
-        local_data="$3"
+        printf "$rootfile_destination already exists. "
+        printf "If it is empty or with wrong data, please remove the folder and run this script again.\n"
+    elif [ $LOAD_TYPE = "local" ]; then
         if [ "$local_data" = "" ]; then
             local_data=$DATA/EPS-HEP2021
         fi
         if ! [ -e $local_data ]; then
             echo "The data source does not exist: " $local_data
-            echo "Try again with `./init.sh rootfiles link SOURCE_FOLDER`"
-            exit 1
+            echo "Try again with './init.sh rootfiles local SOURCE_FOLDER'."
+            return 1
         fi
         ln -s $DATA/EPS-HEP2021 $rootfile_destination
-    elif [ "$2" = "remote" ]; then
+    elif [ $LOAD_TYPE = "remote" ]; then
         server_path=/data_ilc/flc/kunath/LLR-ILD/EPS-HEP2021/
         if ssh  -o ConnectTimeout=1 llrgate01.in2p3.fr "[ -d $server_path ]"; then
             echo "Start copying contents from $CODE_DIR"
@@ -52,9 +52,13 @@ elif [ "$1" = "rootfiles" ]; then
             echo "Else, please check manually that $server_path exists on the remote."
         fi
     else
-        echo "Unexpected second argument: $2."
+        echo "Unexpected second argument: $LOAD_TYPE."
     fi
+    # Unpack the data if necessary.
     for tar_path in $rootfile_destination/*.tar.gz; do
+        if ! [ -e $tar_path ]; then  # Land here when the pattern matches no file.
+            continue
+        fi
         tar_name=$(basename $tar_path .tar.gz)
         if ! [ -d $rootfile_destination/$tar_name ]; then
             tar -xzf $tar_path --directory $rootfile_destination/
@@ -63,6 +67,12 @@ elif [ "$1" = "rootfiles" ]; then
             rm $tar_path
         fi
     done
+}
+
+if [ "$1" = "" ]; then
+    :
+elif [ "$1" = "rootfiles" ]; then
+    search_for_rootfiles "${@:2}"
 else
     echo "Unexpected first argument: $1."
 fi
